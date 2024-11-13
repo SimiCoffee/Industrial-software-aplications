c461160c55339d97754b6b29c2e8421a192bc874
Simikao <s.wierzejski.420@studms.ug.edu.pl>
2024-11-13 09:37:45 +0100

    init(lab04)

---
+++ lab04/src/main/java/ug/proj/warframe/WarframeApplication.java
@@ -0,0 +1,13 @@
+package ug.proj.warframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarframeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarframeApplication.class, args);
+	}
+
+}
